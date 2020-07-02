324356a144250c5d028db4aa36ae45e8a7e6a81d
Renzo Ezagui <renzo.ezagui@gmail.com>
2020-07-02 14:38:40 -0400

    Chapter 18

--- src/app/01-edicion-basica/03-comentarios.ts
+++ src/app/01-edicion-basica/03-comentarios.ts
@@ -13,9 +13,9 @@ const b = 20;
 const c = { a, b };
 
 
-const a = 30;
-const b = 40;
-const c = { a, b };
+// const a = 30;
+// const b = 40;
+// const c = { a, b };
 
 
 console.log( a, b, c );
--- src/app/01-edicion-basica/08-deshacer-rehacer.ts
+++ src/app/01-edicion-basica/08-deshacer-rehacer.ts
@@ -11,7 +11,9 @@
 */
 
 
-
+function holaMundo(){
+    return 'Saludos a todos!';
+}
 
 
 
